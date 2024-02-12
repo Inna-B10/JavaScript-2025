4c20b68ee0a4382b00b2d0dee872616b4fd6cebb
Inna-B10 <inna.b10.kodehode@gmail.com>
2024-02-12 15:50:04 +0100

    draft

--- data/soundsJSexample.js
+++ data/soundsJSexample.js
@@ -1,7 +1,14 @@
+// export const sounds = [
+//         { file: "pathTofile", key: "keyboardKeyTobePressed" },
+//         { file: "pathTofile", key: "keyboardKeyTobePressed" },
+//         { file: "pathTofile", key: "keyboardKeyTobePressed" },
+//         { file: "pathTofile", key: "keyboardKeyTobePressed" },
+// ]
+/* -------------------------------------------------------------------------- */
 
 export const sounds = [
-        { file: "pathTofile", key: "keyboardKeyTobePressed" },
-        { file: "pathTofile", key: "keyboardKeyTobePressed" },
-        { file: "pathTofile", key: "keyboardKeyTobePressed" },
-        { file: "pathTofile", key: "keyboardKeyTobePressed" },
-]
+  { file: "/notes/haunted-mansion-3.wav", key: "c" },
+  { file: "pathTofile", key: "keyboardKeyTobePressed" },
+  { file: "pathTofile", key: "keyboardKeyTobePressed" },
+  { file: "pathTofile", key: "keyboardKeyTobePressed" },
+];
